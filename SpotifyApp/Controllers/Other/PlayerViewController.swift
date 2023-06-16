9ea6babe4920d673c8505db2834d00b9ccea17d0
Aidin Ahmadian <ahmadian_aidin@yahoo.com>
2023-06-16 14:32:32 +0330

    Playback Presenter Artwork

--- SpotifyApp/Controllers/Other/PlayerViewController.swift
+++ SpotifyApp/Controllers/Other/PlayerViewController.swift
@@ -6,13 +6,23 @@
 //
 
 import UIKit
+import SDWebImage
+
+protocol PlayerViewControllerDelegate: AnyObject {
+    func didTapPlayPause()
+    func didTapForward()
+    func didTapBackward()
+    func didSlideSlider(_ value: Float)
+}
 
 class PlayerViewController: UIViewController {
+    
+    weak var dataSource: PlayerDataSource?
+    weak var delegate: PlayerViewControllerDelegate?
     
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
-        imageView.backgroundColor = .blue
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
@@ -24,13 +34,15 @@ class PlayerViewController: UIViewController {
         view.backgroundColor = .systemBackground
         view.addSubview(imageView)
         view.addSubview(controlsView)
-        //controlsView.delegate = self
+        controlsView.delegate = self
         controlsView.translatesAutoresizingMaskIntoConstraints = false
         configureBarButtons()
+        configure()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        
         imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
         imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         imageView.widthAnchor.constraint(equalToConstant: 300).isActive = true
@@ -39,6 +51,11 @@ class PlayerViewController: UIViewController {
         controlsView.topAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
         controlsView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         controlsView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+    }
+    private func configure() {
+        imageView.sd_setImage(with: dataSource?.imageURL, completed: nil)
+        controlsView.configure(with: PlayerControlsViewViewModel(title: dataSource?.songName, subtitle: dataSource?.subtitle))
     }
     
     private func configureBarButtons() {
@@ -53,5 +70,27 @@ class PlayerViewController: UIViewController {
     @objc private func didTapAction() {
         //Actions
     }
+    
+    func refreshUI() {
+        configure()
+    }
 
+}
+
+extension PlayerViewController: PlayerControlsViewDelegate {
+    func playerControlsViewDidTapPlayPauseButton(_ playerControlView: PlayerControlsView) {
+        delegate?.didTapPlayPause()
+    }
+    
+    func playerControlsViewDidTapForwardButton(_ playerControlView: PlayerControlsView) {
+        delegate?.didTapForward()
+    }
+    
+    func playerControlsViewDidTapBackwardsButton(_ playerControlView: PlayerControlsView) {
+        delegate?.didTapBackward()
+    }
+    
+    func playerControlsView(_ playerControlsView: PlayerControlsView, didSlideSlider value: Float) {
+        delegate?.didSlideSlider(value)
+    }
 }

40ba6091e1c87ea03d0f282d262408fa45013122
Aidin Ahmadian <ahmadian_aidin@yahoo.com>
2023-06-16 15:19:20 +0330

    Playing Music

--- SpotifyApp/Presenter/PlaybackPresenter.swift
+++ SpotifyApp/Presenter/PlaybackPresenter.swift
@@ -7,7 +7,7 @@
 
 import AVFoundation
 import Foundation
-import UIKit
+import  UIKit
 
 protocol PlayerDataSource: AnyObject {
     var songName: String? { get }
@@ -16,28 +16,29 @@ protocol PlayerDataSource: AnyObject {
 }
 
 final class PlaybackPresenter {
-    
     static let shared = PlaybackPresenter()
     
     private var track: AudioTrack?
     private var tracks = [AudioTrack]()
     
+    var index = 0
+    
     var currentTrack: AudioTrack? {
         if let track = track, tracks.isEmpty {
             return track
         }
-        else if !tracks.isEmpty {
-            return tracks.first
+        else if let player = self.playerQueue ,!tracks.isEmpty {
+            return tracks[index]
         }
         return nil
     }
     
+    var playerVC: PlayerViewController?
     var player: AVPlayer?
+    var playerQueue: AVQueuePlayer?
     
-    func startPlayback(
-        from viewController: UIViewController,
-        track: AudioTrack
-    ) {
+    func startPlayback(from viewController: UIViewController, track: AudioTrack ) {
+        
         guard let url = URL(string: track.preview_url ?? "") else {
             return
         }
@@ -48,9 +49,11 @@ final class PlaybackPresenter {
         let vc = PlayerViewController()
         vc.title = track.name
         vc.dataSource = self
+        vc.delegate = self
         viewController.present(UINavigationController(rootViewController: vc), animated: true) { [weak self] in
             self?.player?.play()
         }
+        self.playerVC = vc
     }
     
     func startPlayback(
@@ -59,10 +62,75 @@ final class PlaybackPresenter {
     ) {
         self.tracks = tracks
         self.track = nil
+        
+        self.playerQueue = AVQueuePlayer(items: tracks.compactMap ({
+            guard let url = URL(string: $0.preview_url ?? "") else {
+                return nil
+            }
+            return AVPlayerItem(url: url)
+        }))
+        self.playerQueue?.volume = 10
+        self.playerQueue?.play()
+        
         let vc = PlayerViewController()
+        vc.dataSource = self
+        vc.delegate = self
         viewController.present(UINavigationController(rootViewController: vc), animated: true, completion: nil)
+        self.playerVC = vc
+    }
+}
+
+extension PlaybackPresenter: PlayerViewControllerDelegate {
+    func didTapPlayPause() {
+        if let player = player {
+            if player.timeControlStatus == .playing {
+                player.pause()
+            }
+            else if player.timeControlStatus == .paused {
+                player.play()
+            }
+        }
+        else if let player = playerQueue {
+            if player.timeControlStatus == .playing {
+                player.pause()
+            }
+            else if player.timeControlStatus == .paused {
+                player.play()
+            }
+        }
     }
     
+    func didTapForward() {
+        if tracks.isEmpty {
+            //Not playlist or album
+            player?.pause()
+        }
+        else if let player = playerQueue {
+            player.advanceToNextItem()
+            index += 1
+            print(index)
+            playerVC?.refreshUI()
+        }
+    }
+    
+    func didTapBackward() {
+        if tracks.isEmpty {
+            //Not playlist or album
+            player?.pause()
+            player?.play()
+        }
+        else if let firstItem = playerQueue?.items().first {
+            playerQueue?.pause()
+            playerQueue?.removeAllItems()
+            playerQueue = AVQueuePlayer(items: [firstItem])
+            playerQueue?.play()
+            playerQueue?.volume = 10
+        }
+    }
+    
+    func didSlideSlider(_ value: Float) {
+        player?.volume = value
+    }
 }
 
 extension PlaybackPresenter: PlayerDataSource {
